ad5890c8ce80a73c9a95273e4527db8265e0d451
Bernardo Tonin <btprates2004@gmail.com>
2024-05-28 08:40:16 -0300

    feat: create zapimoveis scraper

---
+++ src/scraper/zap.ts
@@ -0,0 +1,14 @@
+import { fetchFromZap, getZapUrl } from "../lib/utils.js";
+import { saveMany } from "../repository/PropertyRepository.js";
+
+export const ZapScrape = async () => {
+  let from = 0;
+  let size = 110;
+
+  for (let i = 0; i < 23; i++) {
+    const result = await saveMany(await fetchFromZap(from, size))
+    console.log(`${result} | ${i}`)
+    //console.log(await fetchFromZap(from, size))
+    from += size - 2;
+  }
+}
