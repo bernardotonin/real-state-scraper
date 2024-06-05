a93a285abe2853a58722779120c9ea1fc8c03fc7
Bernardo Tonin <btprates2004@gmail.com>
2024-06-05 11:41:54 -0300

    feat: implement viva scrape

--- src/scraper/viva.ts
+++ src/scraper/viva.ts
@@ -1,32 +1,42 @@
 import { ProxyAgent, VivaHeaders } from "../lib/constants.js";
-import { getVivaUrl, parseZapJson } from "../lib/utils.js";
+import { getVivaUrl, parseJson } from "../lib/utils.js";
+import { save, findDuplicate } from "../repository/PropertyRepository.js";
 import { PrismaProperty } from "../lib/interfaces.js";
 import { ZapJSON } from "../lib/json.js";
-import fetch from 'node-fetch'
+import fetch from "node-fetch";
 
 export const getVivaCount = async (): Promise<number> => {
   const response = await fetch(getVivaUrl(0, 0), {
     agent: ProxyAgent,
     headers: VivaHeaders,
   });
-  const json = (await response.json()) as ZapJSON;
+  const json = await response.json() as ZapJSON;
   return Math.floor(json.search.totalCount / 108);
 };
 
-export const fetchFromViva = async (from: number, size: number): Promise<PrismaProperty[]> => {
-  const response = await fetch(getVivaUrl(from, size), {agent: ProxyAgent, headers: VivaHeaders});
-  const json = await response.json() as ZapJSON
-  return parseZapJson(json.search.result.listings)
-}
+export const fetchFromViva = async (
+  from: number,
+  size: number
+): Promise<PrismaProperty[]> => {
+  const response = await fetch(getVivaUrl(from, size), {
+    agent: ProxyAgent,
+    headers: VivaHeaders,
+  });
+  const json = await response.json() as ZapJSON;
+  return parseJson(json.search.result.listings);
+};
 
 export const VivaScrape = async () => {
   let from = 0;
   let size = 110;
 
   for (let i = 0; i <= await getVivaCount(); i++) {
-    // const result = await saveMany(await fetchFromZap(from, size))
-    // console.log(`${result} | ${i}`)
-    console.log(await fetchFromViva(from, size))
+    const data = await fetchFromViva(from, size);
+    for (let j = 0; j < data.length; j++) {
+      if (await findDuplicate(data[j])) continue;
+      await save(data[j]);
+    };
     from += size - 2;
+    console.log('sucesso | ' + i);
   }
-}
+};
