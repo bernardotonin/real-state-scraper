95e7b3ce62cf0606ad64833be9622c303c067702
Bernardo Tonin <btprates2004@gmail.com>
2024-06-05 11:41:38 -0300

    refactor: change scraping method to avoid duplicates

--- src/scraper/zap.ts
+++ src/scraper/zap.ts
@@ -1,34 +1,27 @@
-import { getPropertyType, getPropertyUsage, getSalePrice, getZapUrl } from "../lib/utils.js";
-import { ZapHeaders, ProxyAgent } from '../lib/constants.js';
-import { Listing, ZapJSON } from "../lib/json.js";
+import { getZapUrl, parseJson } from "../lib/utils.js";
+import { ZapHeaders, ProxyAgent } from "../lib/constants.js";
+import { save, findDuplicate } from "../repository/PropertyRepository.js";
 import { PrismaProperty } from "../lib/interfaces.js";
-import { saveMany } from "../repository/PropertyRepository.js";
-import fetch from 'node-fetch';
+import { ZapJSON } from "../lib/json.js";
+import fetch from "node-fetch";
 
-
-export const parseZapJson = (listings: Listing[]): PrismaProperty[] => {
-  const parsedProperties: PrismaProperty[] = [];
-  listings.forEach((listing) => {
-    parsedProperties.push({
-      area: listing.listing.usableAreas[0] ? listing.listing.usableAreas[0] : 0,
-      bairro: listing.listing.address.neighborhood,
-      preco: getSalePrice(listing.listing.pricingInfos),
-      quartos: listing.listing.bedrooms[0] ? listing.listing.bedrooms[0] : 0,
-      tipo: getPropertyType(listing.listing.unitTypes[0]),
-      uso: getPropertyUsage(listing.listing.usageTypes[0])
-    });
+const fetchFromZap = async (
+  from: number,
+  size: number
+): Promise<PrismaProperty[]> => {
+  const response = await fetch(getZapUrl(from, size), {
+    agent: ProxyAgent,
+    headers: ZapHeaders,
   });
-  return parsedProperties;
-};
-
-const fetchFromZap = async (from: number, size: number): Promise<PrismaProperty[]> => {
-  const response = await fetch(getZapUrl(from, size), {agent: ProxyAgent, headers: ZapHeaders});
   const json = await response.json() as ZapJSON;
-  return parseZapJson(json.search.result.listings);
+  return parseJson(json.search.result.listings);
 };
 
-const getZapCount = async (): Promise<number> => {
-  const response = await fetch(getZapUrl(0, 1), {agent: ProxyAgent, headers: ZapHeaders});
+export const getZapCount = async (): Promise<number> => {
+  const response = await fetch(getZapUrl(0, 3), {
+    agent: ProxyAgent,
+    headers: ZapHeaders,
+  });
   const json = await response.json() as ZapJSON;
   return Math.floor(json.search.totalCount / 108);
 };
@@ -36,9 +29,13 @@ const getZapCount = async (): Promise<number> => {
 export const ZapScrape = async () => {
   let from = 0;
   let size = 110;
-
   for (let i = 0; i <= await getZapCount(); i++) {
-    await saveMany(await fetchFromZap(from, size));
+    const data = await fetchFromZap(from, size);
+    for (let j = 0; j < data.length; j++) {
+      if (await findDuplicate(data[j])) continue;
+      await save(data[j]);
+    };
     from += size - 2;
-  };
+    console.log('sucesso | ' + i);
+  }
 };
