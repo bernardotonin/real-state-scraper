447f6d3c0a842cf795ab6ba8f92c4c189cee5a18
Bernardo Tonin <btprates2004@gmail.com>
2024-06-05 11:42:20 -0300

    feat: implement findduplicates

--- src/repository/PropertyRepository.ts
+++ src/repository/PropertyRepository.ts
@@ -1,22 +1,34 @@
 import { PrismaProperty } from "../lib/interfaces.js";
-import { create, createMany } from "../service/PropertyService.js";
+import {
+  create,
+  createMany,
+  findByFields,
+} from "../service/PropertyService.js";
 
-export const save = async (property : PrismaProperty): Promise<Boolean> => {
+export const save = async (property: PrismaProperty): Promise<Boolean> => {
   const db_entry = await create(property);
 
   if (db_entry) {
     return true;
-  };
+  }
 
   return false;
-}
+};
 
-export const saveMany = async (properties : PrismaProperty[]): Promise<Boolean> => {
+export const saveMany = async (
+  properties: PrismaProperty[]
+): Promise<Boolean> => {
   const db_entry = await createMany(properties);
 
   if (db_entry) {
     return true;
-  };
+  }
 
   return false;
-}
+};
+
+export const findDuplicate = async (
+  property: PrismaProperty
+): Promise<boolean> => {
+  return await findByFields(property);
+};
